f27fecdd758051a4162c00832cd86d791f2afda7
Mariusz Baran <mariusz.baran23@gmail.com>
2024-01-20 14:48:36 +0100

    Downgrade minimum required dependency versions

--- src/DeezNuts.Hangfire.ApplicationInsights/ServiceCollectionExtensions.cs
+++ src/DeezNuts.Hangfire.ApplicationInsights/ServiceCollectionExtensions.cs
@@ -5,9 +5,20 @@ namespace DeezNuts.Hangfire.ApplicationInsights;
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds necessary Hangfire filters to service collection.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
     public static IServiceCollection AddHangfireApplicationInsights(this IServiceCollection services)
         => services.AddHangfireApplicationInsights(_ => { });
     
+    /// <summary>
+    /// Adds necessary Hangfire filters to service collection.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
+    /// <param name="configureOptions">A delegate to configure <see cref="HangfireApplicationInsightsOptions"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
     public static IServiceCollection AddHangfireApplicationInsights(
         this IServiceCollection services,
         Action<HangfireApplicationInsightsOptions> configureOptions)
@@ -20,6 +31,12 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Applies Hangfire job filters responsible for proper telemetry tracking.
+    /// </summary>
+    /// <param name="configuration">The <see cref="IGlobalConfiguration"/> instance.</param>
+    /// <param name="serviceProvider">The <see cref="IServiceProvider"/> instance used to resolve filters.</param>
+    /// <returns>The <see cref="IGlobalConfiguration"/>.</returns>
     public static IGlobalConfiguration UseApplicationInsights(
         this IGlobalConfiguration configuration, IServiceProvider serviceProvider)
         => configuration
